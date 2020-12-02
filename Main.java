e4b08e8281fd1e6b0b5ca6c80a1de7c2d8856117
SobiaF <25015478+SobiaF@users.noreply.github.com>
2020-12-02 14:38:29 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package learningjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2020";
+        System.out.println("myString is equal to " + myString);
+        String
+    }
+}
